49f92bde1ff7a0c6623089dce8a248137d74127e
Nishank Tripathi <nishank.tripathi@cradlepoint.com>
2021-08-31 22:45:37 +0530

    Polymorphism example added

---
+++ OOPs/Polymorphism/MethodOverloading4.java
@@ -0,0 +1,13 @@
+class MethodOverloading4
+{
+	public static void main(String [] args)
+	{
+		System.out.println("System's main");
+		
+	}
+	public static void main(int [] args)
+	{
+		System.out.println("overloaded main");
+		
+	}
+}
